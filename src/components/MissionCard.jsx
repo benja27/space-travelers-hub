04d4e987ebb455c47f9b9e8cd89baab004f81867
sylvester wamaya <sylvesterwamaya@gmail.com>
2023-06-27 19:07:05 +0300

    Add status buttons to the mission

--- src/components/MissionCard.jsx
+++ src/components/MissionCard.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
 
 const MissionCard = ({name, description}) => {
+    const member = false
+    const join = false
   return (
  
       <tr>
         <td>{name}</td>
         <td className='col-9'>{description}</td>
-        <td></td>
-        <td></td>
+        <td>
+            {
+            member?(
+            <button type='button'>Active member</button>):(
+            <button type='button'>NOT A MEMBER</button>)
+            }           
+            
+        </td>
+        <td>
+        {
+            join?(
+            <button type='button'>Join Mission</button>):(
+            <button type='button'>Leave Mission</button>)
+            }   
+        </td>
       </tr>
    
   )
